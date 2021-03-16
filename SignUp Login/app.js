b3aef002b1286b3c2a9ea2076b415245a137d506
Gaurav Singh <gp5in9h@gmail.com>
2021-03-16 23:04:52 +0530

    movement of bubble enabled

--- SignUp Login/app.js
+++ SignUp Login/app.js
@@ -1,9 +1,14 @@
 const list = document.querySelectorAll('.list-elem');
-
-
+const bar = document.querySelector(".activeSelection");
+let values = list[0].getBoundingClientRect();
+bar.style.left = `${values.x-15}px`;
+bar.style.top = `${values.y+25}px`;
+bar.style.width = `${values.width + 25}px`;
 for (let i of list){
     i.addEventListener('click', () => {
         let values = i.getBoundingClientRect();
-        console.log(values);
+        bar.style.left = `${values.x-15}px`;
+        bar.style.top = `${values.y+25}px`;
+        bar.style.width = `${values.width + 25}px`;
     });
 }
