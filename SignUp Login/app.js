0d7c5b9348914e296c2d6ce169b3ea89cceb771b
Gaurav Singh <gp5in9h@gmail.com>
2021-03-17 02:21:44 +0530

    tables for layout

--- SignUp Login/app.js
+++ SignUp Login/app.js
@@ -8,7 +8,7 @@ for (let i of list){
     i.addEventListener('click', () => {
         let values = i.getBoundingClientRect();
         bar.style.left = `${values.x-15}px`;
-        bar.style.top = `${values.y+25}px`;
+        bar.style.top = `${values.y+30}px`;
         bar.style.width = `${values.width + 25}px`;
     });
 }
