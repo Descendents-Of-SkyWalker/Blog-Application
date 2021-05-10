38da7a025eb05f47f04f95682b7e828e584e8c0f
Rajvi Jasani <rajvijasani1@gmail.com>
2021-05-10 21:50:45 +0530

    Changed color palette

--- Access/scripts/appSignup.js
+++ Access/scripts/appSignup.js
@@ -1,5 +1,5 @@
 // records the interests that a person enters (atleast 3 are required)
-function interestRecorder(){
+function interestRecorder() {
     interestsBuilder();
     let inputs = document.querySelectorAll(".choice");
     let counter = 0;
@@ -7,19 +7,20 @@ function interestRecorder(){
     let choices = []; // choices of the user will be registered onto this;
     next.innerText = "Submit";
     next.disabled = true;
-    for(let i of inputs){
-        i.addEventListener('click', (event)=>{
+    for (let i of inputs) {
+        i.addEventListener('click', (event) => {
             event.preventDefault();
-            i.style.borderColor = "rgba(0, 14, 143)";
+            i.style.borderStyle = "solid";
+            i.style.borderColor = "#25a18e";
             i.style.color = "white";
-            i.style.backgroundColor = "rgba(0, 14, 143, 0.7)";
+            i.style.backgroundColor = "#004e6483";
             // makes sure only one click per button is recorded
-            if (!i.isSelectedFlag){
+            if (!i.isSelectedFlag) {
                 i.isSelectedFlag = true;
                 counter++;
                 choices.push(i.innerText);
             }
-            if (counter>=3 && !flag){
+            if (counter >= 3 && !flag) {
                 flag = true;
                 listDone(iteration);
                 next.disabled = false;
@@ -38,13 +39,13 @@ const bar = document.querySelector(".activeSelection");
 let values = list[0].getBoundingClientRect();
 const points = document.querySelectorAll(".outer-point");
 
-bar.style.left = `${values.x-15}px`;
-bar.style.top = `${values.y+40}px`;
+bar.style.left = `${values.x - 15}px`;
+bar.style.top = `${values.y + 45}px`;
 bar.style.width = `${values.width + 30}px`;
 
-let aboutYouKeys = ["Name","Age","Gender","Bio"];
-let credentialsKeys = ["Username","Mail ID","Password"];
-let addressKeys = ["State","City","PIN Code","Address"];
+let aboutYouKeys = ["Name", "Age", "Gender", "Bio"];
+let credentialsKeys = ["Username", "Mail ID", "Password"];
+let addressKeys = ["State", "City", "PIN Code", "Address"];
 
 let iteration = 0;
 let flagInterests = false;
@@ -55,23 +56,23 @@ const next = document.querySelector("#next");
 dataBuilder(aboutYouKeys);
 let dataPackager = new FormData();
 
-next.addEventListener('click', (event)=>{
+next.addEventListener('click', (event) => {
     event.preventDefault();
-    if (iteration < 3 && extractor(dataPackager)){
-            iteration = syncShift(iteration);
-            switch (iteration) {
-                case 1:
-                    dataBuilder(addressKeys);
-                    break;
-                case 2:
-                    dataBuilder(credentialsKeys);
-                    break;
-                case 3:
-                    if (!flagInterests){
-                        flagInterests = true;
-                        interestRecorder();
-                    }
-                    break;
-            }
+    if (iteration < 3 && extractor(dataPackager)) {
+        iteration = syncShift(iteration);
+        switch (iteration) {
+            case 1:
+                dataBuilder(addressKeys);
+                break;
+            case 2:
+                dataBuilder(credentialsKeys);
+                break;
+            case 3:
+                if (!flagInterests) {
+                    flagInterests = true;
+                    interestRecorder();
+                }
+                break;
+        }
     }
 });
--- Access/scripts/builder.js
+++ Access/scripts/builder.js
@@ -1,33 +1,33 @@
 // common data builder for all elements except interests on signup page
-function dataBuilder(data){
+function dataBuilder(data) {
     let container = document.querySelector("#signUpLogin");
     let table = document.createElement("table");
     table.id = "inputs-table";
-    for(let i=0;i<data.length;i++){
+    for (let i = 0; i < data.length; i++) {
         let tr = document.createElement("tr");
         let td_1 = document.createElement("td");
         let td_2 = document.createElement("td");
         let p = document.createElement("p");
         let input;
-        if(data[i] == "Bio" || data[i] == "Address"){
+        if (data[i] == "Bio" || data[i] == "Address") {
             input = document.createElement("textarea");
             input.type = "text";
             input.className = "text-box";
             input.id = "textarea";
         }
-        else{
+        else {
             input = document.createElement("input");
-            if(data[i].includes("Password"))
+            if (data[i].includes("Password"))
                 input.type = "password";
             else if (data[i].includes("Mail"))
                 input.type = "mail";
-            else if (data[i].includes("Age") || data[i].includes("PIN")){
+            else if (data[i].includes("Age") || data[i].includes("PIN")) {
                 input.type = "number";
-                if (data[i].includes("Age")){
+                if (data[i].includes("Age")) {
                     input.min = '18';
                     input.max = '100';
                 }
-                else if (data[i].includes("PIN")){
+                else if (data[i].includes("PIN")) {
                     input.max = '999999';
                     input.min = '99999';
                 }
@@ -47,25 +47,25 @@ function dataBuilder(data){
     container.appendChild(table);
 }
 
-function bubbleMover(iterator){
+function bubbleMover(iterator) {
     let values = list[iterator].getBoundingClientRect();
-    bar.style.left = `${values.x-15}px`;
-        bar.style.top = `${values.y+40}px`;
-        bar.style.width = `${values.width + 30}px`;
+    bar.style.left = `${values.x - 15}px`;
+    bar.style.top = `${values.y + 45}px`;
+    bar.style.width = `${values.width + 30}px`;
 }
 
 // builds the interests page
-function interestsBuilder(){
+function interestsBuilder() {
     let container = document.querySelector("#signUpLogin");
-    let selection = ["Fun","Philosophy","Work","Inspiration","Lesuire",
-                    "Recreational","Suspense","Music","Market","Recent Trends"];
+    let selection = ["Fun", "Philosophy", "Work", "Inspiration", "Lesuire",
+        "Recreational", "Suspense", "Music", "Market", "Recent Trends"];
     let p = document.createElement("p");
     let inputs = document.createElement("div");
     inputs.classList.toggle("containment");
     inputs.className = "containment";
     p.innerText = "Select Atleast 3 as choices";
     p.style.textAlign = "center";
-    for(let i=0;i<selection.length;i++){
+    for (let i = 0; i < selection.length; i++) {
         let btn = document.createElement("button");
         btn.innerText = selection[i];
         btn.classList.toggle("choice");
@@ -76,16 +76,16 @@ function interestsBuilder(){
 }
 
 // shifts color from blue to green on circles
-function listDone(done){
+function listDone(done) {
     points[done].classList.toggle("current");
     points[done].classList.toggle("done");
 }
 // shifts color from white to blue on circles
-function listCurrent(current){
+function listCurrent(current) {
     points[current].classList.toggle("current");
 }
 // combines functions to simply the process
-function syncShift(iterator){
+function syncShift(iterator) {
     listDone(iterator);
     iterator++;
     listCurrent(iterator);
