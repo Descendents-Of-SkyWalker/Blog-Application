2ee0b8915e94d2994e00d1bf000db40823f76939
Gaurav Singh <gp5in9h@gmail.com>
2021-06-06 15:45:00 +0530

    added stuff

--- Backend/database.php
+++ Backend/database.php
@@ -1,13 +1,5 @@
 <?php
-    // define("DB_HOST", "localhost:3306");
-    // define("DB_USER", "root");
-    // define("DB_PASSWORD", "");
-    // define("DB_DATABASE", "Vibranium");
     header("Access-Control-Allow-Origin: *");
-
-    // $db = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
-    // if (mysqli_connect_errno())
-    //     echo "Database failed";
     class Database{
         private $host = "localhost:3307";
         private $user = "root";
